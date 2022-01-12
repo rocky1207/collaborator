c0a5b170d50ccbf9e474194e2ada4f4c57e3064b
rocky1207 <mjrocky1918@gmail.com>
2022-01-12 11:41:13 +0100

    [CLB-31] Add translation

--- src/components/TimesheetDetails/index.tsx
+++ src/components/TimesheetDetails/index.tsx
@@ -3,8 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import './timesheetDetails.module.css';
 import styles from './timesheetDetails.module.css';
+import { useTranslation } from 'react-i18next';
 
 const TimesheetDetails = () => {
+    const { t } = useTranslation();
     return (
         <section className={styles.timesheetDetailsSection}>
             <p className={styles.date}>04/01/2022</p>
@@ -13,16 +15,18 @@ const TimesheetDetails = () => {
                 <div className={styles.articleDatas}>
                     <h3 className={styles.articleH3}>project name</h3>
                     <p className={styles.articleP}>
-                        Client:{' '}
+                        {t('description.client')}:{' '}
                         <span className={styles.articleSpan}>Company Name</span>
                     </p>
                     <p className={styles.articleP}>
-                        Time Spent:{' '}
+                        {t('description.timeSpent')}:{' '}
                         <span className={styles.articleSpan}>3</span>
                     </p>
                 </div>
                 <div className={styles.descriptionDiv}>
-                    <p className={styles.articleP}>Description:</p>
+                    <p className={styles.articleP}>
+                        {t('description.description')}:
+                    </p>
                     <textarea className={styles.articleTextarea}></textarea>
                 </div>
                 <div>
